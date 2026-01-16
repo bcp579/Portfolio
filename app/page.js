bce6617c2a341d63861fa8b3c50a685b8134e438
Bhavya Patel <bhavyapatel@Bhavyas-MacBook-Pro.local>
2026-01-16 12:35:00 -0500

    Update icon

--- app/page.js
+++ app/page.js
@@ -79,7 +79,6 @@ export default function Portfolio() {
       <>
         Hi! I&apos;m Bhavya, a Computer Science student at TMU with a passion for mobile development and data intelligence. As a CS major with a problem-solving superpower, I tackle technical challenges from a fresh, user-centric perspective.
         <br />
-        <br />
         In my free time, I like discovering new tech, building cross-platform apps, and figuring out my life.
       </>
       ),
@@ -257,7 +256,7 @@ export default function Portfolio() {
             </p>
             <div className="flex space-x-4">
               <a href="#contact" className="bg-[hsl(225,69%,61%)] hover:bg-[hsl(225,69%,79%)] text-white font-bold py-3 px-6 rounded-lg transition-transform transform hover:scale-105 shadow-lg">
-                Contact Me ⌯⌲
+                Contact Me <span className="text-2xl">⌯⌲</span>
               </a>
               <a href="/Portfolio/Bhavya_Resume.pdf" target="_blank" rel="noopener noreferrer" className="border-2 border-[hsl(225,69%,61%)] hover:bg-[hsl(225,69%,61%)] text-[hsl(225,69%,61%)] hover:text-white font-bold py-3 px-6 rounded-lg transition-all transform hover:scale-105">
                 Download Resume
